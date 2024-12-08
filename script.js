368a7ca1d250d2597efb70657ac039008366adf5
Ali Raza <devaliraza74@gmail.com>
2024-12-08 18:09:59 +0500

    add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector(".btn2").onclick = () => {
+    window.location.href = "contact.html"
+}
+
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", () =>
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+);
+
+const menu = document.querySelector(".menu");
+const toggleMenu = () => menu.classList.toggle("active");
+
+document.querySelector(".menu-btn").addEventListener("click", toggleMenu)
+document.querySelector(".close-btn").addEventListener("click", toggleMenu)
+
+document.querySelectorAll(".menu a").forEach((link) => link.addEventListener("click", toggleMenu));
